29c9d2336d6038b1a97415f908d827d4c04216cd
amdsaad <saadamd@gmail.com>
2021-03-06 13:43:47 +0400

    auth0

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,25 +1,25 @@
 export default {
-  mode: 'spa',
+  mode: "spa",
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
@@ -31,7 +31,22 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: [],
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth-next"],
+  auth: {
+    redirect: {
+      login: "/", // redirect user when not connected
+      callback: "/auth/signed-in",
+    },
+    strategies: {
+      auth0: {
+        domain: "amdsaad.us.auth0.com",
+        clientId: "AxExiXY1zcTbjuF94BNDmXr8nWk4xZMR",
+      },
+    },
+  },
+  router: {
+    middleware: ["auth"],
+  },
   /*
    ** Build configuration
    */
---
+++ pages/auth/signed-in.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      this.$auth.loginWith("auth0");
+    },
+  },
+};
+</script>
+
+<style></style>
